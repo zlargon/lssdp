295d5ee842fd2be7090e6a0945264824170063af
leon_huang <leon_huang@gemteks.com>
2015-02-20 00:45:56 +0800

    implement internal function lssdp_log, lssdp_debug, lssdp_error

--- lssdp.c
+++ lssdp.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "lssdp.h"
+
+#define LSSDP_MESSAGE_MAX_LEN 2048
+
+#define lssdp_debug(fmt, agrs...) lssdp_log("DEBUG", __LINE__, __func__, fmt, ##agrs)
+#define lssdp_error(fmt, agrs...) lssdp_log("ERROR", __LINE__, __func__, fmt, ##agrs)
+
+static int lssdp_log(const char * level, int line, const char * func, const char * format, ...);
+
 
 /** Global Variable **/
 static int (* lssdp_log_callback)(const char * file, const char * tag, const char * level, int line, const char * func, const char * message) = NULL;
@@ -14,8 +23,29 @@ int lssdp_set_log_callback(int (* callback)(const char * file, const char * tag,
 // Dummy Function
 void lssdp_hello() {
 #ifdef __OSX__
-    puts("hello OSX");
+    lssdp_error("hello OSX\n");
 #else
-    puts("hello");
+    lssdp_debug("hello\n");
 #endif
+}
+
+
+/** Internal Function **/
+
+static int lssdp_log(const char * level, int line, const char * func, const char * format, ...) {
+    if (lssdp_log_callback == NULL) {
+        return -1;
+    }
+
+    char message[LSSDP_MESSAGE_MAX_LEN] = {0};
+
+    // create message by va_list
+    va_list args;
+    va_start(args, format);
+    vsnprintf(message, LSSDP_MESSAGE_MAX_LEN, format, args);
+    va_end(args);
+
+    // invoke log callback function
+    lssdp_log_callback(__FILE__, "SSDP", level, line, func, message);
+    return 0;
 }
