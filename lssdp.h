b1d1e3a5b51d4b704366771ab3945aac4221e24c
leon_huang <leon_huang@gemteks.com>
2015-03-02 11:50:37 +0800

    update README.md and function description

--- lssdp.h
+++ lssdp.h
@@ -66,6 +66,11 @@ typedef struct lssdp_ctx {
 /*
  * 01. lssdp_network_interface_update
  *
+ * update network interface.
+ *
+ * Note:
+ *  - lssdp.interface, lssdp.interface_num will be updated.
+ *
  * @param lssdp
  * @return = 0      success
  *         < 0      failed
@@ -75,7 +80,13 @@ int lssdp_network_interface_update(lssdp_ctx * lssdp);
 /*
  * 02. lssdp_socket_create
  *
- * if SSDP socket is exist, close the socket first then create.
+ * create SSDP socket.
+ *
+ * Note:
+ *  - SSDP port must be setup ready before call this function. (lssdp.port > 0)
+ *  - if SSDP socket is already exist (lssdp.sock > 0),
+ *    the socket will be closed, and create a new one.
+ *  - SSDP neighbor list will be force clean up.
  *
  * @param lssdp
  * @return = 0      success
@@ -86,7 +97,11 @@ int lssdp_socket_create(lssdp_ctx * lssdp);
 /*
  * 03. lssdp_socket_close
  *
- * when socket close, SSDP neighbor list will be force clean up.
+ * close SSDP socket.
+ *
+ * Note:
+ *  - if SSDP socket <= 0, will be ignore, and lssdp.sock will be set to -1.
+ *  - SSDP neighbor list will be force clean up.
  *
  * @param lssdp
  * @return = 0      success
@@ -97,6 +112,17 @@ int lssdp_socket_close(lssdp_ctx * lssdp);
 /*
  * 04. lssdp_socket_read
  *
+ * read SSDP socket.
+ *
+ * 1. if read success, packet_received_callback will be invoked.
+ * 2. if received SSDP packet is match to Search Target (lssdp.header.search_target),
+ *     - M-SEARCH: send RESPONSE back
+ *     - NOTIFY/RESPONSE: add/update to SSDP neighbor list
+ *
+ * Note:
+ *  - SSDP socket and port must be setup ready before call this function. (sock, port > 0)
+ *  - if SSDP neighbor list has been changed, neighbor_list_changed_callback will be invoked.
+ *
  * @param lssdp
  * @return = 0      success
  *         < 0      failed
@@ -105,6 +131,11 @@ int lssdp_socket_read(lssdp_ctx * lssdp);
 
 /*
  * 05. lssdp_send_msearch
+ *
+ * send SSDP M-SEARCH packet to multicast address (239.255.255.250)
+ *
+ * Note:
+ *  - SSDP port must be setup ready before call this function. (lssdp.port > 0)
  *
  * @param lssdp
  * @return = 0      success
@@ -115,6 +146,11 @@ int lssdp_send_msearch(lssdp_ctx * lssdp);
 /*
  * 06. lssdp_send_notify
  *
+ * send SSDP NOTIFY packet to multicast address (239.255.255.250)
+ *
+ * Note:
+ *  - SSDP port must be setup ready before call this function. (lssdp.port > 0)
+ *
  * @param lssdp
  * @return = 0      success
  *         < 0      failed
@@ -124,6 +160,12 @@ int lssdp_send_notify(lssdp_ctx * lssdp);
 /*
  * 07. lssdp_neighbor_check_timeout
  *
+ * check neighbor in list is timeout or not. (lssdp.neighbor_timeout)
+ * the timeout neighbor will be remove from the list.
+ *
+ * Note:
+ *  - if neighbor be removed, neighbor_list_changed_callback will be invoked.
+ *
  * @param lssdp
  * @return = 0      success
  *         < 0      failed
@@ -132,6 +174,8 @@ int lssdp_neighbor_check_timeout(lssdp_ctx * lssdp);
 
 /*
  * 08. lssdp_set_log_callback
+ *
+ * setup SSDP log callback. All SSDP library log will be forward to here.
  *
  * @param callback
  * @return = 0    success
